e277670de4e639eb3e659fc11bf1c579a8952ebf
Sigge Brandt <siggebrandt@icloud.com>
2025-10-15 23:44:16 +0200

    Nav IMG

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,10 @@
 import "./Nav.css";
+import eggsellent_logo from "./eggsellent_logo.png";
 
 function Nav() {
   return (
     <nav>
+      <img src={eggsellent_logo} className="nav-logo" alt="logo" />
       <p>
         <center>Eggsellent Eggs</center>
       </p>
